3b0193bc3e2f53159d41a25bed982dc46b5a2ded
ahallerod <90375472+ahallerod@users.noreply.github.com>
2022-02-21 14:04:51 +0100

    fixes

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ todoTemplate.remove();
 const todoList = document.querySelector('#todo-list');
 const newTodo = document.querySelector('#new-item');
 const newForm = document.querySelector('#input-form');
+const counter = document.querySelector('#counter');
 
 newForm.onsubmit = event => {
     createNewTodo(event);
@@ -15,14 +16,30 @@ newForm.onsubmit = event => {
 function createNewTodo(event){
     event.preventDefault();
 
+    if(newTodo.value == '') return;
+
     let newTodoItem = todoTemplate.content.firstElementChild.cloneNode(true);
     newTodoItem.querySelector('.todo-text').textContent = newTodo.value;
     todoList.append(newTodoItem);
 
+    // Update lists
+    allItems = document.querySelectorAll('.todo-item');
+    allCheckboxes = document.querySelectorAll('.todo-item .todo-checkbox');
+    updateCounter()
+
     const deleteButton = newTodoItem.querySelector('.todo-delete');
     deleteButton.onclick = event => {
         newTodoItem.remove();
+        allItems = document.querySelectorAll('.todo-item');
+        allCheckboxes = document.querySelectorAll('.todo-item .todo-checkbox');
+        updateCounter()
     }
+
+    const checkbox = newTodoItem.querySelector('.todo-checkbox');
+    checkbox.onclick = event => {
+        updateCounter();
+    }
+
     
 }
 
@@ -35,60 +52,68 @@ const completedButton = document.querySelector('#hide-notCompleted');
 const clearCompletedButton = document.querySelector('#clear-completed');
 
 let allItems = document.querySelectorAll('.todo-item');
+//const allItems1 = () => document.querySelectorAll('.todo-item'); Andreas Experiment
 let allCheckboxes = document.querySelectorAll('.todo-item .todo-checkbox');
 
 
 //Add boolean value to check if items are hidden or not? Change button to checkbox?
 showAllButton.onclick = event => {
-    showAll(event);
-
+    showAllItems();
+    clearFilterActiveStyles()
     showAllButton.classList.add('active');
-    showAllButton.classList.remove('filter-button');
-    
-
+    //showAllButton.classList.remove('filter-button');
 }
 
 activeButton.onclick = event =>{
-    hideCompleted(event);
+    hideCompleted();
+    clearFilterActiveStyles()
+    activeButton.classList.add('active');
+    //activeButton.classList.remove('filter-button');
 }
 
 completedButton.onclick = event =>{
-    hideNotCompleted(event);
+    hideNotCompleted();
+    clearFilterActiveStyles()
+    completedButton.classList.add('active');
+}
+
+function clearFilterActiveStyles(){
+    showAllButton.classList.remove('active');
+    activeButton.classList.remove('active');
+    completedButton.classList.remove('active');
 }
 
 clearCompletedButton.onclick = event => {
     removeCompleted(event);
 }
 
-function showAll(event){
-    event.preventDefault();
-
-
+function showAllItems(){
     for(let item of allItems){
         item.classList.remove('hidden');
     }
-
 }
 
-function hideCompleted(event){
-    event.preventDefault();
+function updateCounter(){
+    const uncheckedItems = document.querySelectorAll('.todo-checkbox:not(:checked)');
+    if(uncheckedItems.length == 1){
+        counter.textContent = '1 item left';
+    } else {
+        counter.textContent = uncheckedItems.length + ' items left';
+    }
+}
 
-
+function hideCompleted(){
+    showAllItems()
     for(var i = 0; i < allCheckboxes.length; i++){
         if(allCheckboxes[i].checked == true){
-            // allItems[i].remove();
-            // allItems[i].style.display = 'none';
             allItems[i].classList.add('hidden');
-            
         }
     }
 }
 
 
-function hideNotCompleted(event){
-    event.preventDefault();
-
-
+function hideNotCompleted(){
+    showAllItems()
     for(var i = 0; i < allCheckboxes.length; i++){
         if(allCheckboxes[i].checked == false){
             // allItems[i].remove();
@@ -100,13 +125,9 @@ function hideNotCompleted(event){
 function removeCompleted(event){
     event.preventDefault();
 
-
     for(var i = 0; i < allCheckboxes.length; i++){
         if(allCheckboxes[i].checked == true){
-            // allItems[i].remove();
-            // allItems[i].style.display = 'none';
             allItems[i].remove();
-            
         }
     }
 }
